fe101161de4e95ac7fa71163ff53727303d2b66d
jadmax0w0 <jadmax@qq.com>
2026-02-10 17:59:27 +0800

    eval_docvqa.sh: DocVQA 测评脚本
    llava.py: 修改 LMMs-Eval 中的代码，修复了模型评测时无法正常输出 `<|im_end|>` 而导致输出正确答案以外的额外内容的问题
    
    predice.py: 添加针对 `max_new_tokens` 的命令行参数设置

--- predict.py
+++ predict.py
@@ -62,7 +62,7 @@ def predict(args):
             temperature=args.temperature,
             top_p=args.top_p,
             num_beams=args.num_beams,
-            max_new_tokens=256,
+            max_new_tokens=args.max_new_tokens,
             use_cache=True)
 
         outputs = tokenizer.batch_decode(output_ids, skip_special_tokens=True)[0].strip()
@@ -83,6 +83,7 @@ if __name__ == "__main__":
     parser.add_argument("--temperature", type=float, default=0.2)
     parser.add_argument("--top_p", type=float, default=None)
     parser.add_argument("--num_beams", type=int, default=1)
+    parser.add_argument("--max_new_tokens", type=int, default=256)
     args = parser.parse_args()
 
     predict(args)
