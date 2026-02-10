5c8a66c113f68e51b6c728788c9e957bb85c6a95
jadmax0w0 <jadmax@qq.com>
2026-02-10 20:48:59 +0800

    eval_gqa, eval_mmmu: 两个 bench 的评测脚本

--- scripts/eval/eval_mmmu.sh
+++ scripts/eval/eval_mmmu.sh
@@ -1,13 +1,15 @@
-export CUDA_VISIBLE_DEVICES=2,3,4,5
+export CUDA_VISIBLE_DEVICES=2,3,4,5,6,7
 
-NUM_PROCESSES=4
+NUM_PROCESSES=6
 MAIN_PROCESS_PORT=30100
 
+TASK_NAME=mmmu_val
+RUN_NAME=fvlm_qwen2_stg2
 # CKPT_PATH=ckpt/llava_fastvithd_0.5b_stage3/checkpoint-79226
 CKPT_PATH=/data2/lyx/ckpts/fastvlm/llava_fastvithd_0.5b_stage2
 RESULT_DIR=eval/result_mmmu
 
-export FVLM_CKTIME=0
+export FVLM_CKTIME=0  # 控制模型检查 prepare input 和 llm forward 耗时
 
 python -m accelerate.commands.launch \
     --num_processes $NUM_PROCESSES \
@@ -15,10 +17,11 @@ python -m accelerate.commands.launch \
     -m lmms_eval \
     --model llava \
     --model_args pretrained=${CKPT_PATH},conv_template=qwen_2 \
-    --tasks mmmu_val \
+    --tasks $TASK_NAME \
     --batch_size 1 \
+    --gen_kwargs until="<|im_end|>" \
     --log_samples \
-    --log_samples_suffix fvlm_qwen2_stg3 \
+    --log_samples_suffix $RUN_NAME \
     --output_path ${RESULT_DIR}/log
 
 ## LLaVA args in LLMs-Eval
